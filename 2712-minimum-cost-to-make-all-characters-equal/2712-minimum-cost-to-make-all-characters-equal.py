5372a77c770a351894ba6dba35e76f66030a772f
sujit-2005 <venkatsujit.chinthala@gmail.com>
2026-02-26 10:05:29 +0530

    Time: 63 ms (93.88%), Space: 20 MB (30.61%) - LeetHub

--- 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.py
+++ 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.py
@@ -1,25 +1,10 @@
 class Solution:
-    def invert_substring(self, s, l, r):
-        sub = s[l:r+1]
-        inverted = sub.translate(str.maketrans('01', '10'))
-        return s[:l] + inverted + s[r+1:]
-
     def minimumCost(self, s: str) -> int:
-        count = 0
         n = len(s)
-        mid = n // 2
-        slow = 0
-        while slow < n - 1:
-            fast = slow + 1
-            if s[slow] != s[fast]:
-                if slow < mid:
-                    # flip prefix
-                    s = self.invert_substring(s, 0, slow)
-                    count += (slow + 1)
-                else:
-                    # flip suffix
-                    s = self.invert_substring(s, fast, n - 1)
-                    count += (n - fast)
-            slow += 1
-
-        return count
+        cost = 0
+        
+        for i in range(1, n):
+            if s[i] != s[i - 1]:
+                cost += min(i, n - i)
+                
+        return cost
