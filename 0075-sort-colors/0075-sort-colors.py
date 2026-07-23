c344b7db109cdc0be5d53f89b8097f79f4133697
sujit-2005 <venkatsujit.chinthala@gmail.com>
2026-07-23 17:50:33 +0530

    Time: 0 ms (100%), Space: 19.2 MB (60.65%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,15 +1,16 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        slow = 0
-
-        # First pass: move all 0s to the front
-        for fast in range(len(nums)):
-            if nums[fast] == 0:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
-                slow += 1
-
-        # Second pass: move all 1s after the 0s
-        for fast in range(slow, len(nums)):
-            if nums[fast] == 1:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
-                slow += 1
+        left=0
+        right=len(nums)-1
+        i=0
+        while i<=right:
+            if nums[i]==0:
+                nums[i],nums[left]=nums[left],nums[i]
+                left+=1
+            elif nums[i]==2:
+                nums[i],nums[right]=nums[right],nums[i]
+                right-=1
+                i-=1
+            i+=1
+        
+            
