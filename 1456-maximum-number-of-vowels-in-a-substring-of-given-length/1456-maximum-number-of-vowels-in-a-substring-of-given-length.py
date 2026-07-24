96cbc5c5aaf063bd720788fa69768adfb5a73799
sujit-2005 <venkatsujit.chinthala@gmail.com>
2026-07-24 18:28:45 +0530

    Time: 36 ms (98.85%), Space: 19.7 MB (65.53%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,12 +1,23 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        window=0
-        for i in range(k):
-            if s[i]=="a" or s[i]=="e" or s[i]=="i" or s[i]=="o" or s[i]=="u":
-                window+=1
-            ans=window
-        for j in range(k,len(s)):
-            window+=1 if s[j]=="a" or s[j]=="e" or s[j]=="i" or s[j]=="o" or s[j]=="u" else 0
-            window-=1 if s[j-k]=="a" or s[j-k]=="e" or s[j-k]=="i" or s[j-k]=="o" or s[j-k]=="u" else 0
-            ans =max(window,ans)
-        return ans
+        
+        v = "aeiou"
+        c = 0
+
+        for l in s[:k]:
+            if l in v:
+                c += 1
+        
+        maxV = c
+
+        for i in range(k,len(s)):
+            if s[i] in v:
+                c += 1
+            
+            if s[i-k] in v:
+                c -= 1
+
+            if c > maxV:
+                maxV = c
+        
+        return maxV
